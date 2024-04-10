c230fa0fd874b8b0661dafc455f94470c5764b66
LeoFV87 <153769315+LeoFV87@users.noreply.github.com>
2024-04-10 22:17:54 +0000

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       MetodosOrdenamiento burbuja= new MetodosOrdenamiento();
+       //Creo el arreglo de eneteros int [];
+       int [] numeros= {30,7,12,5};
+
+       //Mandar a imprimir mi arreglo
+       burbuja.imprimir(numeros);
+
+       int [] arregloOrdenadoBuble =burbuja.sortByBubble(numeros);
+       burbuja.imprimir(arregloOrdenadoBuble);
+      
+
     }
 }
